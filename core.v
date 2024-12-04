5d1907bd3a12cb4715fa1803f18bcc0d41980f26
mingi001025 <102572844+mingi001025@users.noreply.github.com>
2024-12-04 13:12:11 -0800

    Add files via upload

---
+++ core.v
@@ -0,0 +1,165 @@
+module core ( clk, inst, ofifo_valid, D_xmem, Q_out, reset);
+
+parameter bw = 4;
+parameter row = 8;
+parameter col = 8;
+parameter psum_bw = 16;
+
+
+input clk;
+input reset;
+input [49:0] inst; 
+output ofifo_valid;
+input [bw*row-1:0]D_xmem;
+output [col*psum_bw-1:0] Q_out;
+
+wire CEN_xmem;
+wire WEN_xmem;
+wire [10:0] A_xmem;
+
+wire CEN_omem;
+wire WEN_omem;
+wire [10:0] A_omem;
+
+wire CEN_pmem ;
+wire WEN_pmem;
+wire [10:0] A_pmem;
+wire ofifo_rd ;
+wire ififo_wr;
+wire ififo_rd;
+wire l0_rd ;
+wire l0_wr;
+wire execute;
+wire load;
+wire acc;
+wire mode;
+wire data_mode;
+
+assign output_loading_mode = inst[49];
+
+assign CEN_omem     = inst[48];
+assign WEN_omem     = inst[47];
+assign A_omem       = inst[46:36];
+
+assign mode         = inst[35];
+assign data_mode    = inst[34];
+assign acc          = inst[33];
+assign CEN_pmem     = inst[32];
+assign WEN_pmem     = inst[31];
+assign A_pmem       = inst[30:20];
+assign CEN_xmem     = inst[19];
+assign WEN_xmem     = inst[18];
+assign A_xmem       = inst[17:7];
+assign ofifo_rd     = inst[6];
+assign ififo_wr     = inst[5];
+assign ififo_rd     = inst[4];
+assign l0_rd        = inst[3];
+assign l0_wr        = inst[2];
+assign execute      = inst[1];
+assign load         = inst[0];
+
+wire [31:0 ]Q_act;
+wire [31:0 ]Q_wt;
+wire [127:0 ]Q_out;
+
+reg [bw*col-1:0] l0_in;
+wire [bw*col-1:0] ififo_in;
+wire [col*psum_bw-1: 0] ofifo_out; 
+
+// Sram 1 Instantiation for L0
+
+ sram_32b_w2048 activation_sram (
+	.CLK(clk), 
+	.CEN(CEN_xmem), 
+	.WEN(WEN_xmem),
+   .A(A_xmem), 
+   .D(D_xmem), 
+   .Q(Q_act));
+
+/////////////////////////////////
+
+///////////////////////////////////////////////////
+
+// Sram 1 Instantiation for IFIFO
+
+ sram_32b_w2048 weight_sram (
+	.CLK(clk), 
+	.CEN(CEN_pmem), 
+	.WEN(WEN_pmem),
+   .A(A_pmem), 
+   .D(D_xmem), 
+   .Q(Q_wt));
+
+wire [127:0] sfp_out;
+/////////////////////////////////
+wire [col*psum_bw-1: 0] ofifo_sram_in;
+assign ofifo_sram_in = output_loading_mode ? sfp_out : ofifo_out;
+
+// Sram Instantiation for OFIFO
+  parameter num = 3000;
+ sram_128b_w2048 ofifo_sram (
+	.CLK(clk), 
+	.CEN(CEN_omem), 
+	.WEN(WEN_omem),
+   .A(A_omem), 
+   .D(ofifo_sram_in), 
+   .Q(Q_out));
+
+
+/////////////////////////////////
+
+
+/////////// Corelet Instantation /////////////////
+
+corelet core_inst1 (
+    .clk(clk),
+    .l0_in(l0_in),
+    .l0_rd(l0_rd),
+    .mode(mode),
+    .data_mode(data_mode),
+    .l0_wr(l0_wr),
+    .ififo_in(ififo_in),
+    .ififo_rd(ififo_rd),
+    .ififo_wr(ififo_wr),
+    .ofifo_out(ofifo_out),
+    .ofifo_rd(ofifo_rd),
+    .load(load),
+    .execute(execute),
+    .Q_out(Q_out),
+    .acc(acc),
+    .sfp_out(sfp_out),
+    .output_loading_mode(output_loading_mode),
+    .reset(reset)
+
+);
+
+// output or wt stationary
+
+always @(*)
+begin
+    if(mode)    // Weight Stationary
+    begin
+        
+        if(data_mode) // Send in Weight/ Activation
+            l0_in = Q_wt;  // If data mode is 1, send wt to L0
+        else                  
+            l0_in = Q_act; // If data mode is 0, send act to L0
+
+    end
+    else       // Output Stationary
+    begin
+    
+
+    end
+end
+// assign l0_in = Q_act;
+// assign ififo_in = Q_wt;
+
+
+always @(posedge clk) begin
+
+   // l0_in <= Q_act;
+
+end
+
+endmodule
---
+++ corelet.v
@@ -0,0 +1,163 @@
+module corelet (clk, l0_in, l0_rd, mode, data_mode, l0_wr, Q_out, acc, sfp_out,
+                reset, ififo_in, ififo_rd, load, execute, ififo_wr, ofifo_out, ofifo_rd, output_loading_mode);
+  
+  parameter row  = 8;
+  parameter bw = 4;
+  parameter col = 8; 
+  parameter psum_bw = 16;
+
+  
+  input  clk;
+  input [bw*col-1:0] l0_in;
+  input [bw*col-1:0] ififo_in;
+  input ififo_rd;
+  input l0_rd;
+  input ififo_wr;
+  input l0_wr;
+  input reset;
+  input acc;
+  input load;
+  input [127:0] Q_out;
+  input execute;
+  input output_loading_mode;
+  input mode;
+  input data_mode;
+  output [127:0] sfp_out;
+  input ofifo_rd;
+  output [col*psum_bw-1: 0]  ofifo_out;
+
+  wire l0_ready;
+  wire l0_full;
+  wire ififo_ready;
+  wire ififo_full;
+  
+  reg l0_wr_q;
+  reg ififo_wr_q;
+
+
+  wire  [row*bw-1:0] l0_out;
+  wire  [row*bw-1:0] ififo_out;
+  
+  ////////////// L0 Instance /////////////////////
+
+    l0 #(.bw(bw)) l0_instance 
+  (
+        .clk(clk),
+        .in(l0_in), 
+        .out(l0_out), 
+        .rd(l0_rd),
+        .data_mode(data_mode),
+        .wr(l0_wr_q), 
+        .o_full(l0_full), 
+        .reset(reset), 
+        .o_ready(l0_ready)
+  );
+ //////////////////////////////////////////////////////
+
+ //////////////// IFIFO Instance ///////////////////////
+
+    l0 #(.bw(bw)) IFIFO_instance 
+  (
+        .clk(clk),
+        .in(ififo_in), 
+        .out(ififo_out), 
+        .rd(ififo_rd),
+        .data_mode(data_mode),
+        .wr(ififo_wr_q), 
+        .o_full(ififo_full), 
+        .reset(reset), 
+        .o_ready(ififo_ready)
+  );
+
+
+ //////////////////////////////////////////////////////
+
+wire [psum_bw*col-1:0] out_s;
+
+wire [row*bw-1:0] in_w;
+wire [psum_bw*col-1:0] in_n;
+wire [1:0] inst_w;
+wire [col-1:0] valid;
+reg [col-1:0] valid_q;
+
+
+wire [row*(bw+1)-1:0] zero_out;
+zero_detector_row zero_inst(
+  .in(l0_out),
+  .out(zero_out),
+  .clk(clk),
+  .reset(reset)
+);
+
+
+ //////////////// Mac Array Instance ///////////////////
+
+mac_array mac_array_inst (
+  .clk(clk), 
+  .reset(reset), 
+  .out_s(out_s), 
+  .in_w(zero_out),
+  .mode(mode),
+  .data_mode(data_mode),
+  .in_n(128'b0), 
+  .inst_w({execute, load}), 
+  .valid(valid)
+  
+  );
+     
+
+ /////////////////////////////////////////////////////
+
+
+
+ // wire [col*psum_bw-1: 0] ofifo_out; 
+  wire ofifo_valid;   
+  wire ofifo_full; 
+
+
+
+ //////////////// Ofifo Instance ///////////////////////
+
+
+ofifo ofifo_inst (
+  .clk(clk), 
+  .in(out_s), 
+  .out(ofifo_out), 
+  .rd(ofifo_rd), 
+  .wr(valid_q), 
+  .o_full(ofifo_full), 
+  .reset(reset), 
+  .o_ready(ofifo_ready), 
+  .o_valid(ofifo_valid)
+);
+
+
+//////////////////////////////////////////////////////////
+
+
+
+/////////////////////// SFP ///////////////////////////////
+reg acc_q;
+sfp_row sfp_inst (
+    .clk(clk),    // Clock signal
+    .reset(reset),  // Reset signal
+    .acc(acc_q),    // Accumulate enable
+    .in(Q_out), // Input data bus
+    .out(sfp_out) // Output data bus after ReLU
+
+);
+
+//////////////////////////////////////////////////////////
+
+
+
+  always @(posedge clk)
+  begin
+    l0_wr_q <= l0_wr; 
+    ififo_wr_q <= ififo_wr;
+    acc_q <= acc;
+    valid_q <= valid;
+
+  end
+
+endmodule
